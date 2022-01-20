3960f5b74983090441d097017a647dadecb0c1b6
johnbox <johnbox@thatbox>
2022-01-20 12:19:06 -0300

    setup store

--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -1,6 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
 import accountReducer from './reducers'
-// ...
+
+const sagaMiddleware = createSagaMiddleware()
+import logger from 'redux-logger'
+import { watcherSaga } from '../sagas/rootSaga'
 
 export const store = configureStore({
     reducer: {
@@ -8,10 +12,14 @@ export const store = configureStore({
         // comments: commentsReducer,
         // users: usersReducer,
     },
-    devTools: true
+    devTools: true,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false })
+        .concat(sagaMiddleware)
     // devTools: (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 })
 
+
+sagaMiddleware.run(watcherSaga)
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
